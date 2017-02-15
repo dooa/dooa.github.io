1bc44744682c9ef6adbfec7d8d12fc3d1fb27e2f
dooa <dooa@users.noreply.github.com>
2017-02-15 23:16:20 +0200

    Update jscrypt.js

--- gnot/scripts/javascrypt/jscrypt.js
+++ gnot/scripts/javascrypt/jscrypt.js
@@ -56,35 +56,35 @@ function setKey() {
 }
 
 function Generate_seed() {
-    	var i, j, k = "";
-	
-	addEntropyTime();
-	var seed = keyFromEntropy();
-	
-    	var prng = new AESprng(seed);
-	if (document.seed.keytype[0].checked) {
-	    //	Text key
-	    var charA = ("A").charCodeAt(0);
-	    
-	    for (i = 0; i < 12; i++) {
-		if (i > 0) {
-	    	    k += "-";
-		}
-		for (j = 0; j < 5; j++) {
-	    	    k += String.fromCharCode(charA + prng.nextInt(25));
-		}
-	    }
-	} else {
-	    // Hexadecimal key
-	    var hexDigits = "0123456789ABCDEF";
-	    
-	    for (i = 0; i < 64; i++) {
-	    	k += hexDigits.charAt(prng.nextInt(15));
-	    }
-	}
-    	document.seed.text.value = k;
-	delete prng;
+    var i, j, k = "";
+
+    addEntropyTime();
+    var seed = keyFromEntropy();
+
+    var prng = new AESprng(seed);
+    if (document.seed.keytype[0].checked) {
+        //	Text key
+        var charA = ("A").charCodeAt(0);
+
+        for (i = 0; i < 12; i++) {
+            if (i > 0) {
+                k += "-";
+            }
+            for (j = 0; j < 5; j++) {
+                k += String.fromCharCode(charA + prng.nextInt(25));
+            }
+        }
+    } else {
+        // Hexadecimal key
+        var hexDigits = "0123456789ABCDEF";
+
+        for (i = 0; i < 64; i++) {
+            k += hexDigits.charAt(prng.nextInt(15));
+        }
     }
+    document.seed.text.value = k;
+    delete prng;
+}
 
 function Generate_key() {
 	var i,
